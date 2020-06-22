e99c030e723d31bf02150f2891af9162ccf263c8
mykhrd <mykhrd@gmail.com>
2020-06-22 22:40:42 +0900

    add sample image

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Text('Hello World'),
-  ));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[100],
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Image(
+          image: NetworkImage(
+              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+        ),
+      ),
+    ),
+  );
 }
